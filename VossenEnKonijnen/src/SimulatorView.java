873adaffdb129d48e06835c2f38631c103bea0dc
kevin <kevinstier@live.nl>
2015-02-01 20:33:39 +0100

    Aanpassing legenda

--- VossenEnKonijnen/src/SimulatorView.java
+++ VossenEnKonijnen/src/SimulatorView.java
@@ -54,7 +54,7 @@ public class SimulatorView extends JFrame
         stepLabel = new JLabel(STEP_PREFIX, JLabel.CENTER);
         population = new JLabel(POPULATION_PREFIX, JLabel.CENTER);
         
-        logoImage = new ImageIcon(Simulator.class.getResource("/Legenda2.png"));
+        logoImage = new ImageIcon(Simulator.class.getResource("/Legenda3.png"));
         legend = new JLabel(logoImage, JLabel.CENTER);
         
         JPanel panelleft = new JPanel(new BorderLayout());
